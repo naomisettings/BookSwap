c6fa380276b6c863d336f0360a3cf3c7173b6639
sellares.feiner.raimon <sellares.feiner.raimon@alumnat.copernic.cat>
2021-05-06 09:36:40 +0200

    First commit

---
+++ app/src/main/java/cat/copernic/bookswap/MainActivity.kt
@@ -0,0 +1,11 @@
+package cat.copernic.bookswap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
