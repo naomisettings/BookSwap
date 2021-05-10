1899c570608c6a9ca06350134388e287d3c2e06e
Angels <marmol.samper.maria.angeles@alumnat.copernic.cat>
2021-05-10 18:04:29 +0200

    BottonNavigation

--- app/src/main/java/cat/copernic/bookswap/MainActivity.kt
+++ app/src/main/java/cat/copernic/bookswap/MainActivity.kt
@@ -2,10 +2,32 @@ package cat.copernic.bookswap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navView:BottomNavigationView= findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.myNavHostFragment)
+
+        // Passar cada identificador de menú com un conjunt d'identificadors perquè
+        // cada menú s’ha de considerar com a destinacions de primer nivell.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.llistatLlibres,
+                R.id.meusLlibres,
+                R.id.modificarUsuari))
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
     }
+
 }
