a7df5ca9393bcc2b4e0e64afa64920127ae5a509
sellares.feiner.raimon <sellares.feiner.raimon@alumnat.copernic.cat>
2021-05-11 14:41:11 +0200

    Registre per email i amb google (obtenint mail)

--- app/src/main/java/cat/copernic/bookswap/login/LoginFragment.kt
+++ app/src/main/java/cat/copernic/bookswap/login/LoginFragment.kt
@@ -1,22 +1,68 @@
 package cat.copernic.bookswap.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import cat.copernic.bookswap.R
 import cat.copernic.bookswap.databinding.FragmentLoginBinding
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoginFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    private val AUTH_REQUEST_CODE = 2002
+    private var user: FirebaseUser? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
         val binding: FragmentLoginBinding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
 
+        binding.bttnRegistre.setOnClickListener() {
+            findNavController().navigate(R.id.action_loginFragment_to_registreFragment)
+        }
+
+        login()
+
+        val user = FirebaseAuth.getInstance().currentUser
+
+        user?.let {
+            // Name, email address, and profile photo Url
+            val name = user.displayName
+            val email = user.email
+            Log.d("emailnom", name.toString() + email)
+        }
+
         return binding.root
+    }
+
+    private fun login() {
+        val proveedors = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+        startActivityForResult(
+            AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(proveedors)
+                .build(),
+            AUTH_REQUEST_CODE
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == AUTH_REQUEST_CODE) {
+            FirebaseAuth.getInstance().currentUser
+        }
     }
 }
--- app/src/main/java/cat/copernic/bookswap/registre/RegistreViewModel.kt
+++ app/src/main/java/cat/copernic/bookswap/registre/RegistreViewModel.kt
@@ -1,22 +1,11 @@
 package cat.copernic.bookswap.registre
 
 import androidx.lifecycle.ViewModel
+import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.ktx.actionCodeSettings
 
 class RegistreViewModel: ViewModel() {
 
     init{
-        val actionCodeSettings = actionCodeSettings {
-            // URL you want to redirect back to. The domain (www.example.com) for this
-            // URL must be whitelisted in the Firebase Console.
-            url = "https://www.bookswap-7d63c.firebaseapp.com"
-            // This must be true
-            handleCodeInApp = true
-            setAndroidPackageName(
-                "cat.copernic.bookswap",
-                true, /* installIfNotAvailable */
-                "21" /* minimumVersion */)
-        }
-
     }
 }
